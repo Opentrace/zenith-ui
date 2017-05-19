7f3aa07f287097b6b46f48909dceeb8a3434e66d
Mark Rabey <mark.rabey@gshiftlabs.com>
2017-05-19 17:01:36 -0400

    added basic build for documentation:

---
+++ docs/src/app/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from 'react-title-component';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Title render="Zenith UI Documentation" />
+        Documentation for Zenith UI
+      </div>
+    );
+  }
+}
