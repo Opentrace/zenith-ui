6090c1a4d5c41ec57d5a17670a6983566b937673
MarkRabey <mark@markrabey.com>
2017-05-19 09:46:52 -0400

    added basic testing scripts

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const STATUS = {
+  HOVERED: 'hovered',
+  NORMAL: 'normal',
+};
+
 export default class Button extends React.Component {
   constructor(props) {
     super(props);
+
+    this._onMouseEnter = this._onMouseEnter.bind(this);
+    this._onMouseLeave = this._onMouseLeave.bind(this);
+
+    this.state = {
+      class: STATUS.NORMAL
+    };
+  }
+
+  _onMouseEnter() {
+    this.setState({class: STATUS.HOVERED});
+  }
+
+  _onMouseLeave() {
+    this.setState({class: STATUS.NORMAL});
   }
 
   render() {
+    const { children } = this.props;
     return (
-      <button></button>
+      <button
+        className={ this.state.class }
+        onMouseEnter={ this._onMouseEnter }
+        onMouseLeave={ this._onMouseLeave }>
+        { children }
+      </button>
     );
   }
 }
