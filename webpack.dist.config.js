bda30dbf9f140fd4ba1a6f5be2b33fb65393ffcd
Mark <mark@markrabey.com>
2017-08-21 11:07:26 -0400

    updated build scripts

---
+++ webpack.dist.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const path = require('path');
+const config = require('./webpack.config');
+
+config.entry = './src';
+config.output = {
+  path: path.join(__dirname, './dist'),
+  filename: 'index.js',
+  library: 'Zenith UI',
+  libraryTarget: 'commonjs2',
+};
+config.externals = {
+  react: 'react',
+};
+config.target = 'node';
+config.plugins = [
+  new webpack.optimize.UglifyJsPlugin(),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production')
+  }),
+];
+
+module.exports = config;
